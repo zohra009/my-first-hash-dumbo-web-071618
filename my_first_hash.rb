28eb050eb7a8f501c573ca3807877598684bf20e
anzohra@outlook.com <anzohra@outlook.com>
2018-07-10 02:43:04 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -37,11 +37,11 @@ def adding
                  }
   # add 2 muskets to the shipping_manifest hash below
   shipping_manifest.add["muskets"] = 2
-  shipping_manifest["gun powder"] = 4
+  
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+  shipping_manifest["gun powder"] = 4
 
 
   # return the shipping_manifest hash below
