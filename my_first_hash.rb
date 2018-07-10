9c254432b8e1e517a418aa93ce43869b1e781402
anzohra@outlook.com <anzohra@outlook.com>
2018-07-10 02:36:46 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -26,7 +26,7 @@ def retrieval
                  }
 
   # Look up the value of the "oil paintings" key in the shipping_manifest hash below
-puts shipping_manifest["key2"]
+puts shipping_manifest["oil paintings"]
 end
 
 def adding
