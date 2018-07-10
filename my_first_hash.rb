440641f2efc5bbfb866ad2401bab834ca1f19bef
anzohra@outlook.com <anzohra@outlook.com>
2018-07-10 02:44:08 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -36,7 +36,7 @@ def adding
                   "oil paintings" => 3
                  }
   # add 2 muskets to the shipping_manifest hash below
-  shipping_manifest.add["muskets"] = 2
+  shipping_manifest["muskets"] = 2
   
 
 
